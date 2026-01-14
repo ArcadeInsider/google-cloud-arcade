100f0af95aa7b1a1992f8804a54c95fda98fd895
Campus Perks <himeshganorkar9@gmail.com>
2026-01-14 11:49:08 +0530

    Update arcadeinsider-cloudstorageclisdk.sh

--- Cloud Storage : Quick Start - CLISDK/arcadeinsider-cloudstorageclisdk.sh
+++ Cloud Storage : Quick Start - CLISDK/arcadeinsider-cloudstorageclisdk.sh
@@ -1,1 +1,58 @@
 
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+clear
+
+# Welcome message
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}   CAMPUSPERKS | CLOUD STORAGE LAB 🚀   ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo
+
+gcloud auth list
+gcloud config list project
+export PROJECT_ID=$(gcloud config get-value project)
+
+gsutil mb gs://$PROJECT_ID-techcode
+
+curl https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/800px-Ada_Lovelace_portrait.jpg --output ada.jpg
+
+gsutil cp ada.jpg gs://$PROJECT_ID-techcode
+
+rm ada.jpg
+
+gsutil cp -r gs://$PROJECT_ID-techcode/ada.jpg .
+
+gsutil cp gs://$PROJECT_ID-techcode/ada.jpg gs://$PROJECT_ID-techcode/image-folder/
+
+gsutil ls gs://$PROJECT_ID-techcode
+
+gsutil ls -l gs://$PROJECT_ID-techcode/ada.jpg
+
+gsutil acl ch -u AllUsers:R gs://$PROJECT_ID-techcode/ada.jpg
+
+# Final message
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}          🎉 LAB COMPLETED SUCCESSFULLY! 🎉          ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo
+echo "${YELLOW_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}YouTube: https://www.youtube.com/@CampusPerkss${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}🔥 Like • Share • Subscribe for more Arcade Labs${RESET_FORMAT}"
